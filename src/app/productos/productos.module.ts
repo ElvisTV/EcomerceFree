9fee5b4f9ffb0759a293732f4e16f9b670e08c2a
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-07-18 13:18:25 -0500

    creacion de paginas para el directorio producto

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    ProductoComponent,
+    HomeComponent,
+    ListadoComponent
+  ],
   imports: [
     CommonModule
   ]
