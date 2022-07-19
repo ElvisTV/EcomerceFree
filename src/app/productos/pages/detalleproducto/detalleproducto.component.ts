ba0a657a2d799168ef2257486f0e3bbe892e3209
Elvis Arturo Tezen Villanueva <elvis.tezen@softtek.com>
2022-07-19 09:48:23 -0500

    Componente para colocar el detalle de producto

---
+++ src/app/productos/pages/detalleproducto/detalleproducto.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalleproducto',
+  templateUrl: './detalleproducto.component.html',
+  styles: [
+  ]
+})
+export class DetalleproductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/productos/productos-routing.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { DetalleproductoComponent } from './pages/detalleproducto/detalleproducto.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'listado',
+        component: ListadoComponent
+      },
+      {
+        path: 'agregar',
+        component: AgregarComponent
+      },
+      {
+        path: 'editar/:id',
+        component: AgregarComponent
+      },
+      {
+        path: 'buscar',
+        component: BuscarComponent
+      },
+      {
+        path: ':id',
+        component: ProductoComponent
+      },
+      {
+        path: 'detalleproducto',
+        component: DetalleproductoComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'listado'
+      }
+    ]
+  }
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class ProductosRoutingModule { }
